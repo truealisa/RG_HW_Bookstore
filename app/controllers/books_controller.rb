0f3c758fd3606a58cba755f83930751a7720e692
Alisa Truzhenko <truealisa@gmail.com>
2018-08-11 01:20:08 +0300

    Add scss assets; Create Books controller; Add books resources to routes; Add index.haml into books views

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def index   # GET /books
+    @books = Book.all
+  end
+
+  def show    # GET /books/:id
+    # ...
+  end
+
+  def new     # GET /books/new
+    # ...
+  end
+
+  def edit    # GET /books/:id/edit
+    # ...
+  end
+
+  def create  # book /books/
+    # ...
+  end
+
+  def update  # PATCH /books/:id PUT /books/:id
+    # ...
+  end
+
+  def destroy # DELETE /books/:id
+    # ...
+  end
+end
